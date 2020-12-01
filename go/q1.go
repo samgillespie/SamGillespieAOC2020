ad6814f7a5588df036b24c84f16c78d8584dcc5f
Sam Gillespie <sammyseal90@gmail.com>
2020-12-01 18:08:46 +1000

    Some cleanup

--- go/q1.go
+++ go/q1.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	lib "./lib"
 )
 
 func main() {
+	start := time.Now()
+
 	data := lib.ReadInputAsInt(1)
 	part1(data)
 	part2(data)
+	elapsed := time.Since(start)
+
+	log.Printf("Main took %s", elapsed)
 }
 
 func part1(data []int) {
@@ -18,7 +25,7 @@ func part1(data []int) {
 		for j := i + 1; j < len(data); j++ {
 			sum := data[i] + data[j]
 			if sum == 2020 {
-				fmt.Println("Part 1 Solution")
+				fmt.Println("Question 1 Part 1 Solution:")
 				fmt.Println(data[i] * data[j])
 				break
 			}
@@ -30,13 +37,9 @@ func part2(data []int) {
 	for i := 0; i < len(data); i++ {
 		for j := i + 1; j < len(data); j++ {
 			for k := j + 1; k < len(data); k++ {
-				if k <= j {
-					continue
-				}
-
 				sum := data[i] + data[j] + data[k]
 				if sum == 2020 {
-					fmt.Println("Part 2 Solution")
+					fmt.Println("Question 1 Part 2 Solution:")
 					fmt.Println(data[i] * data[j] * data[k])
 					return
 				}
